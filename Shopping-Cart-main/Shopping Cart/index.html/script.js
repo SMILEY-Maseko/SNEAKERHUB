3e92902a539ee17fb3ca17200ce6e50ca67319de
Wakhekile  Maseko <wakhekilemaseko@gmail.com>
2024-06-24 14:06:11 +0200

    update

--- Shopping-Cart-main/Shopping Cart/index.html/script.js
+++ Shopping-Cart-main/Shopping Cart/index.html/script.js
@@ -33,32 +33,3 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
-var cart =[];
-
-function addtocart(a){
-    cart.push({...categories[a]})
-    displaycart();
-}
-
-function displaycart(a){
-    let j = 0
-    if(cart.length==0){
-        document.getElementById('cartItem').innerHTML = "Your Cart Is Empty";
-    }
-    else{
-        document.getElementById("cartItem").innerHTML = cart.map((item)=>)
-        {
-            var {image, title, price} = items;
-            return(
-                `<div class='cart-item'>
-                <div class='row-img'>
-                    <img class='rowing' src=${image}>
-                    </div>
-                    <p style='font-size:12px;'>${title}</p>
-                    <h2 style='font-size: 15px;'>${price}.00</h2>
-              "<i class='fa-solid fa-trash' onclinc='delElement("+ (j++) +")'></i></div>`
-            );
-        }
-        } .join('');
-    }
-}
